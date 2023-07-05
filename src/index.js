56900e36fe3fb7b77b3201c38a30e74184728d9f
Artemis <tregubartemofficial@gmail.com>
2023-07-06 01:49:08 +0300

    change store location to index.js

--- src/index.js
+++ src/index.js
@@ -2,39 +2,28 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { ThemeProvider, createTheme } from "@mui/material";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import store from "./app/store";
+import { configureStore } from "@reduxjs/toolkit";
+import eventSlice from "./app/features/event/eventReducer";
+import modalSlice from "./app/features/modal/modalReducer";
 
-const theme = createTheme({
-  typography: {
-    h1: {
-      fontSize: "3rem",
-      fontWeight: "600",
-    },
-    h2: {
-      fontSize: "1.75rem",
-      fontWeight: "600",
-    },
-    h3: {
-      fontSize: "1.5rem",
-      fontWeight: "600",
-    },
+const store = configureStore({
+  reducer: {
+    events: eventSlice.reducer,
+    modals: modalSlice.reducer,
   },
 });
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
       <BrowserRouter>
         <Provider store={store}>
           <App />
         </Provider>
       </BrowserRouter>
-    </ThemeProvider>
   </React.StrictMode>
 );
 
